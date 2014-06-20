b8dd7cd44a109217d81a40bfa2c89a406ce66fab
Matthias <matthias@eckhart-onlinemedien.com>
2014-06-20 22:42:39 +0200

    Selenium-nightwatch tests added.

--- tests/start.js
+++ tests/start.js
@@ -2,7 +2,7 @@
  * Created by Matthias on 11/06/14.
  */
 module.exports = {
-    "Scrumpy" : function (client) {
+    "Scrumpy title test" : function (client) {
         client
             .url("http://127.0.0.1:3000")
             .waitForElementVisible("body", 1000)
